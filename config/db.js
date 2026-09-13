b00cedbf855f0dd1abd3c0d675d5290208116947
M. Zafid Affan <you@example.com>
2026-09-13 23:36:17 +0700

    fix: update database connection config and remove defaults

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); // <--- WAJIB ADA DI BARIS PERTAMA
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
@@ -6,7 +7,7 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  connectTimeout: 30000 // Menambah waktu timeout jadi 30 detik
+  connectTimeout: 30000
 });
 
 db.connect((err) => {
@@ -14,7 +15,7 @@ db.connect((err) => {
     console.error('🔥 Gagal terkoneksi ke database:', err);
     return;
   }
-  console.log('✅ Berhasil terhubung ke database MySQL');
+  console.log('✅ Berhasil terhubung ke database MySQL Railway');
 });
 
 module.exports = db;
